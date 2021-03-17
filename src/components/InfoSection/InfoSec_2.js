a9519e85dcadc09e86077b3c27434908a8d9f031
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-03-17 04:31:17 -0700

    add aos

--- src/components/InfoSection/InfoSec_2.js
+++ src/components/InfoSection/InfoSec_2.js
@@ -39,24 +39,28 @@ const InfoSec2 = ({
                   <SkillsWrap darkText={darkText}>
                     <div data-aos="zoom-in">
                       <h3>&lt; Languages /&gt;</h3><br />
-                      <p> HTML, CSS, SCSS/SASS, <br />JavaScript(ES6), JQuery, TypeScript,</p><br />
+                      <p> HTML5, CSS3, SCSS/SASS, <br />JavaScript(ES6), TypeScript</p><br />
                     </div>
 
                     <div data-aos="zoom-in" data-aos-delay="100">
                       <h3>&lt; Frameworks ＆ Libraries /&gt;</h3><br />
-                      <p> React, React-Redux, React-Hooks, <br />styled-components, Chart.js </p><br />
+                      <p> React, React-Redux, React-Hooks, <br />styled-components, Chart.js JQuery, <br />Node.js, Express.js</p><br />
                     </div>
 
                     <div data-aos="zoom-in" data-aos-delay="300" id="trigger-left">
-                      <h3>&lt; Databese ＆ Tools /&gt;</h3><br />
-                      <p> Git, Figma, XD, Photoshop, <br />MongoDB, Firebase, Heroku, Postman, Slack</p>
+                      <h3>&lt; Others ＆ Tools /&gt;</h3><br />
+                      <p> Git, Figma, XD, Photoshop, 
+                      <br/>RESTAPI, JSON, Webpack
+                      <br />MongoDB, Firebase, Heroku, Postman, Slack
+                      
+                      </p>
                     </div>
                   </SkillsWrap>
                 </Subtitle>
               </TextWrapper>
             </ColumnSkill>
 
-            <ColumnSkill2 data-aos="fade-left" data-aos-duration="1000" data-aos-delay="50">
+            <ColumnSkill2 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="50">
               <ImgWrapSkill >
                 {/* Styled Icon Part */}
                 <StyledIcon />
