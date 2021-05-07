63f20fd9b023fbb0e176713fb3705979376d133f
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-05-07 01:20:15 -0700

    Changes to be committed:   new file:   1   modified:   src/components/WorkDescription/WorkDesc.js

--- src/components/WorkDescription/WorkDesc.js
+++ src/components/WorkDescription/WorkDesc.js
@@ -68,7 +68,6 @@ const WorkDesc = () => {
                   <LanguageList>
                     <li> mySQL</li>
                     <li> Bootstrap5</li>
-                    <li> WEB API</li>
                     <li> Figma</li>
                     <li> XD</li>
                     <li> Git</li>
